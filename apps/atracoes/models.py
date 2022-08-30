2060bf92d9eda17cfa9452c369db6444e62ce7a3
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-08-30 14:13:13 -0300

    feat: created model Atracao in atracoes

--- apps/atracoes/models.py
+++ apps/atracoes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Atracao(models.Model):
+    nome = models.CharField(max_length=150)
+    descricao = models.TextField()
+    horario_func = models.TextField()
+    idade_minima = models.IntegerField()
+    observacoes = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
