44ddef9f508ce5f0673451611830132b510553a2
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-08-30 15:00:52 -0300

    feat: added relationship fk between PontoTuristico and Endereco

--- apps/core/models.py
+++ apps/core/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from apps.atracoes.models import Atracao
 from apps.avaliacoes.models import Avaliacao
 from apps.comentarios.models import Comentario
+from apps.enderecos.models import Endereco
 
 
 class PontoTuristico(models.Model):
@@ -12,6 +13,7 @@ class PontoTuristico(models.Model):
     atracoes = models.ManyToManyField(Atracao)
     comentarios = models.ManyToManyField(Comentario)
     avaliacoes = models.ManyToManyField(Avaliacao)
+    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.nome
