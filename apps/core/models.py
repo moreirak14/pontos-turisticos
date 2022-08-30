ccc60dd12281ba7b22bb38d087f88682a40dd37e
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-08-30 14:17:49 -0300

    feat: added relationship m2m between PontoTuristico and Atracoes

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+from apps.atracoes.models import Atracao
+
 
 class PontoTuristico(models.Model):
     nome = models.CharField(max_length=150)
     descricao = models.TextField()
     aprovado = models.BooleanField(default=False)
+    atracoes = models.ManyToManyField(Atracao)
 
     def __str__(self):
         return self.nome
