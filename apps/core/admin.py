1c5f1a5906f5a3fe331a9b7c13547b2b62d97816
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-08-31 17:12:00 -0300

    feat: register DocIdentificacao in admin

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import PontoTuristico
+from .models import DocIdentificacao, PontoTuristico
 
+admin.site.register(DocIdentificacao)
 admin.site.register(PontoTuristico)
