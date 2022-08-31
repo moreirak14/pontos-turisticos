9f2d038cd622f4659e86378f5e3ecaf10ac942d4
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-08-31 16:04:12 -0300

    feat: added extra fields in PontoTuristicoSerializer

--- apps/core/api/serializers.py
+++ apps/core/api/serializers.py
@@ -1,9 +1,28 @@
 from rest_framework import serializers
 
+from apps.atracoes.api.serializers import AtracaoSerializer
+from apps.avaliacoes.api.serializers import AvalicaoSerializer
+from apps.comentarios.api.serializers import ComentarioSerializer
 from apps.core.models import PontoTuristico
+from apps.enderecos.api.serializers import EnderecoSerializer
 
 
 class PontoTuristicoSerializer(serializers.ModelSerializer):
+    atracoes = AtracaoSerializer(many=True)
+    comentarios = ComentarioSerializer(many=True)
+    avaliacoes = AvalicaoSerializer(many=True)
+    endereco = EnderecoSerializer()
+
     class Meta:
         model = PontoTuristico
-        fields = ["id", "nome", "descricao", "aprovado", "foto"]
+        fields = [
+            "id",
+            "nome",
+            "descricao",
+            "aprovado",
+            "atracoes",
+            "comentarios",
+            "avaliacoes",
+            "endereco",
+            "foto",
+        ]
